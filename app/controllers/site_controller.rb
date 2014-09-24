716994e274bddc58b546ce0cca46b5ca3f3a3da6
Fernando Serapio <fhernandosc@gmail.com>
2014-09-23 19:58:48 -0500

    implement facebook page creation and feed display using koala gem to retrieve information form Open Graph

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,37 @@
+class SiteController < ApplicationController
+  before_action :set_page, only: [:show]
+
+  def index
+    @pages = Page.all
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.new page_params
+
+    @page.set_fb_page_data params[:page][:facebook_page_id]
+
+    respond_to do |format|
+      if @page.save
+        format.html { redirect_to root_path, notice: 'Facebook Page found and saved!' }
+      else
+        format.html { redirect_to root_path, alert: 'Ooops! Page not found! :(' }
+      end
+    end
+  end
+
+  def show
+    @feed = @page.get_feed()
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_page
+      @page = Page.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def page_params
+      params.require(:page).permit(:facebook_page_id)
+    end
+end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,26 @@
 class Page < ActiveRecord::Base
+  validates :name, :facebook_page_id, :facebook_page_image_url, :facebook_page_url, presence: true
+  validates :facebook_page_id, uniqueness: true
+
+  def set_fb_page_data facebook_id
+    graph = Koala::Facebook::API.new ENV['FB_ACCESS_TOKEN']
+
+    begin
+      page = graph.get_object facebook_id
+      image = graph.get_picture facebook_id
+
+      self.name = page['name']
+      self.facebook_page_id = page['id']
+      self.facebook_page_url = page['link']
+      self.facebook_page_image_url = image
+    rescue Exception => ex
+      puts ex.message
+    end
+  end
+
+  def get_feed
+    graph = Koala::Facebook::API.new ENV['FB_ACCESS_TOKEN']
+
+    graph.get_connections(self.facebook_page_id, 'feed?limit=10')
+  end
 end
